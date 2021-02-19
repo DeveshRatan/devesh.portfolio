6d70d598b7b0cae2467b1762e1f52c8a2011560d
DeveshRatan <59884616+DeveshRatan@users.noreply.github.com>
2021-02-19 15:18:27 +0530

    initial commit : added portfolio
    A portfolio website to showcase my details.

---
+++ main.js
@@ -0,0 +1,75 @@
+function next()
+{
+    document.getElementById("bg").style.background="#C4B5FD";
+    document.getElementById("bg").style.color="#7F1D1D";
+    document.getElementById("bg").style.fontWeight="bold";
+    
+}
+
+function next1()
+{
+    document.getElementById("bg1").style.background="#C4B5FD";
+    document.getElementById("bg1").style.color="#7F1D1D";
+    document.getElementById("bg1").style.fontWeight="bold";
+    
+}
+
+function next2()
+{
+    document.getElementById("bg2").style.background="#C4B5FD";
+    document.getElementById("bg2").style.color="#7F1D1D";
+    document.getElementById("bg2").style.fontWeight="bold";
+    
+}
+
+function next3()
+{
+    document.getElementById("bg3").style.background="#C4B5FD";
+    document.getElementById("bg3").style.color="#7F1D1D";
+    document.getElementById("bg3").style.fontWeight="bold";
+    
+}
+
+function next4()
+{
+    document.getElementById("bg4").style.background="#C4B5FD";
+    document.getElementById("bg4").style.color="#7F1D1D";
+    document.getElementById("bg4").style.fontWeight="bold";
+    
+}
+
+function previous()
+{
+    document.getElementById("bg").style.background="#FFFFFF";
+    document.getElementById("bg").style.color="#111827";
+    document.getElementById("bg").style.fontWeight="normal";
+} 
+
+function previous1()
+{
+    document.getElementById("bg1").style.background="#FFFFFF";
+    document.getElementById("bg1").style.color="#111827";
+    document.getElementById("bg1").style.fontWeight="normal";
+} 
+
+function previous2()
+{
+    document.getElementById("bg2").style.background="#FFFFFF";
+    document.getElementById("bg2").style.color="#111827";
+    document.getElementById("bg2").style.fontWeight="normal";
+} 
+
+function previous3()
+{
+    document.getElementById("bg3").style.background="#FFFFFF";
+    document.getElementById("bg3").style.color="#111827";
+    document.getElementById("bg3").style.fontWeight="normal";
+} 
+
+function previous4()
+{
+    document.getElementById("bg4").style.background="#FFFFFF";
+    document.getElementById("bg4").style.color="#111827";
+    document.getElementById("bg4").style.fontWeight="normal";
+} 
+
